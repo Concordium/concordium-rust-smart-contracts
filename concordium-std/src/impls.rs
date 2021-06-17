0eca5391c04f4929718193b7468b7d5a3faabda6
Kasper Dissing Bargsteen <kb@concordium.com>
2021-06-17 15:31:35 +0200

    Reordering

--- concordium-std/src/impls.rs
+++ concordium-std/src/impls.rs
@@ -691,6 +691,21 @@ impl<K: Serial + Ord> SerialCtx for BTreeSet<K> {
     }
 }
 
+impl<K: Deserial + Ord + Copy> DeserialCtx for BTreeSet<K> {
+    fn deserial_ctx<R: Read>(
+        source: &mut R,
+        size_len: &schema::SizeLength,
+        ensure_ordered: bool,
+    ) -> ParseResult<Self> {
+        let len = schema::deserial_length(source, size_len)?;
+        if ensure_ordered {
+            deserial_set_no_length(source, len)
+        } else {
+            deserial_set_no_length_no_order_check(source, len)
+        }
+    }
+}
+
 impl<K: Serial + Ord, V: Serial> SerialCtx for BTreeMap<K, V> {
     fn serial_ctx<W: Write>(
         &self,
@@ -702,6 +717,21 @@ impl<K: Serial + Ord, V: Serial> SerialCtx for BTreeMap<K, V> {
     }
 }
 
+impl<K: Deserial + Ord + Copy, V: Deserial> DeserialCtx for BTreeMap<K, V> {
+    fn deserial_ctx<R: Read>(
+        source: &mut R,
+        size_len: &schema::SizeLength,
+        ensure_ordered: bool,
+    ) -> ParseResult<Self> {
+        let len = schema::deserial_length(source, size_len)?;
+        if ensure_ordered {
+            deserial_map_no_length(source, len)
+        } else {
+            deserial_map_no_length_no_order_check(source, len)
+        }
+    }
+}
+
 impl<T: Serial> SerialCtx for &[T] {
     fn serial_ctx<W: Write>(
         &self,
@@ -710,6 +740,17 @@ impl<T: Serial> SerialCtx for &[T] {
     ) -> Result<(), W::Err> {
         schema::serial_length(self.len(), size_len, out)?;
         serial_vector_no_length(self, out)
+    }
+}
+
+impl<T: Deserial> DeserialCtx for Vec<T> {
+    fn deserial_ctx<R: Read>(
+        source: &mut R,
+        size_len: &schema::SizeLength,
+        _ensure_ordered: bool,
+    ) -> ParseResult<Self> {
+        let len = schema::deserial_length(source, size_len)?;
+        deserial_vector_no_length(source, len)
     }
 }
 
@@ -734,47 +775,6 @@ impl SerialCtx for String {
     }
 }
 
-impl<K: Deserial + Ord + Copy> DeserialCtx for BTreeSet<K> {
-    fn deserial_ctx<R: Read>(
-        source: &mut R,
-        size_len: &schema::SizeLength,
-        ensure_ordered: bool,
-    ) -> ParseResult<Self> {
-        let len = schema::deserial_length(source, size_len)?;
-        if ensure_ordered {
-            deserial_set_no_length(source, len)
-        } else {
-            deserial_set_no_length_no_order_check(source, len)
-        }
-    }
-}
-
-impl<K: Deserial + Ord + Copy, V: Deserial> DeserialCtx for BTreeMap<K, V> {
-    fn deserial_ctx<R: Read>(
-        source: &mut R,
-        size_len: &schema::SizeLength,
-        ensure_ordered: bool,
-    ) -> ParseResult<Self> {
-        let len = schema::deserial_length(source, size_len)?;
-        if ensure_ordered {
-            deserial_map_no_length(source, len)
-        } else {
-            deserial_map_no_length_no_order_check(source, len)
-        }
-    }
-}
-
-impl<T: Deserial> DeserialCtx for Vec<T> {
-    fn deserial_ctx<R: Read>(
-        source: &mut R,
-        size_len: &schema::SizeLength,
-        _ensure_ordered: bool,
-    ) -> ParseResult<Self> {
-        let len = schema::deserial_length(source, size_len)?;
-        deserial_vector_no_length(source, len)
-    }
-}
-
 impl DeserialCtx for String {
     fn deserial_ctx<R: Read>(
         source: &mut R,
@@ -787,5 +787,3 @@ impl DeserialCtx for String {
         Ok(res)
     }
 }
-
-// TODO: group by type instead of by trait
