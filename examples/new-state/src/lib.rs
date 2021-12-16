f71639bb6b2436e3943035829c7c83ab9bcf8fd6
Kasper Dissing Bargsteen <kb@concordium.com>
2021-12-16 10:28:10 +0100

    Create example with persistable state

--- examples/new-state/src/lib.rs
+++ examples/new-state/src/lib.rs
@@ -1,14 +1,111 @@
+use std::{cell::RefCell, rc::Rc};
+
 use concordium_cis1::*;
 use concordium_std::*;
 
 type TokenId = TokenIdU8;
-type TokenCount = u64;
+type TokenCount = u8;
 
-#[derive(Serial)]
-enum Keys {
-    SimpleMap,
-    TokenState,
-    TokenCount,
+/// Stored as:
+/// 0/next_collection_id :: u64
+/// 1/token_count :: u8
+/// 2(first collection index)/<address> => <map_index (3..n)>
+/// 3..n maps/token_id => token_count :: u8
+#[derive(SchemaType)]
+struct State<S: HasContractStateLL> {
+    token_state:    StateMap<Address, StateMap<TokenId, TokenCount, S>, S>,
+    another_struct: AnotherStruct<S>,
+    total_tokens:   u64,
+    total_tokens_p: Persisted<u64, S>,
+}
+
+struct AnotherStruct<S: HasContractStateLL> {
+    a_set: StateSet<u8, S>,
+}
+
+impl<S> Persistable<S> for AnotherStruct<S>
+where
+    S: HasContractStateLL,
+{
+    fn store(self, prefix: &[u8], state_ll: Rc<RefCell<S>>) {
+        let mut owned_prefix = prefix.to_vec();
+        owned_prefix.push(0);
+        self.a_set.store(&owned_prefix, state_ll)
+    }
+
+    fn load(prefix: &[u8], state_ll: Rc<RefCell<S>>) -> ParseResult<Self> {
+        let mut owned_prefix = prefix.to_vec();
+        owned_prefix.push(0);
+        Ok(Self {
+            a_set: StateSet::load(&owned_prefix, state_ll)?,
+        })
+    }
+}
+
+impl<S> Persistable<S> for State<S>
+where
+    S: HasContractStateLL,
+{
+    fn store(self, prefix: &[u8], state_ll: Rc<RefCell<S>>) {
+        let mut owned_prefix = prefix.to_vec();
+
+        owned_prefix.push(0);
+        self.token_state.store(&owned_prefix, Rc::clone(&state_ll));
+        owned_prefix.pop();
+
+        owned_prefix.push(1);
+        self.another_struct.store(&owned_prefix, Rc::clone(&state_ll));
+        owned_prefix.pop();
+
+        owned_prefix.push(2);
+        self.total_tokens.store(&owned_prefix, Rc::clone(&state_ll));
+        owned_prefix.pop();
+
+        owned_prefix.push(3);
+        self.total_tokens_p.store(&owned_prefix, state_ll);
+    }
+
+    fn load(prefix: &[u8], state_ll: Rc<RefCell<S>>) -> ParseResult<Self> {
+        let mut owned_prefix = prefix.to_vec();
+
+        owned_prefix.push(0);
+        let token_state = StateMap::load(&owned_prefix, Rc::clone(&state_ll))?;
+        owned_prefix.pop();
+
+        owned_prefix.push(1);
+        let another_struct = AnotherStruct::load(&owned_prefix, Rc::clone(&state_ll))?;
+        owned_prefix.pop();
+
+        owned_prefix.push(2);
+        let total_tokens = u64::load(&owned_prefix, Rc::clone(&state_ll))?;
+        owned_prefix.pop();
+
+        owned_prefix.push(3);
+        let total_tokens_lsc = Persisted::load(&owned_prefix, state_ll)?;
+        owned_prefix.pop();
+
+        Ok(Self {
+            token_state,
+            another_struct,
+            total_tokens,
+            total_tokens_p: total_tokens_lsc,
+        })
+    }
+}
+
+#[init(contract = "storable-contract")]
+fn init<S: HasContractStateLL>(
+    _ctx: &impl HasInitContext,
+    allocator: &mut ContractStateHL<S>,
+) -> InitResult<State<S>> {
+    Ok(State {
+        token_state:    allocator.new_map(),
+        another_struct: AnotherStruct {
+            a_set: allocator.new_set(),
+        },
+        total_tokens:   0,
+        total_tokens_p: Persisted::new(0),
+    })
 }
 
 #[derive(Serialize, SchemaType)]
@@ -18,43 +115,17 @@ struct MintParams {
     token_count: TokenCount,
 }
 
-// With GATs (Generic Associated Types), we could avoid the _ parameter and do:
-// init<State: HasContractStateHL> .. {
-//    let simple_map: State::Map<u8,u8> = state.new_map();
-// }
-
-#[init(contract = "new-state", parameter = "MintParams")]
-fn init(_ctx: &impl HasInitContext, state: &mut impl HasContractStateHL) -> InitResult<()> {
-    let simple_map: StateMap<u8, u8, _> = state.new_map();
-    state.insert(Keys::SimpleMap, simple_map);
-
-    let token_state: StateMap<Address, StateMap<TokenId, TokenCount, _>, _> = state.new_map();
-    state.insert(Keys::TokenState, token_state);
-    state.insert(Keys::TokenCount, 0u64);
-    Ok(())
-}
-
-#[receive(contract = "new-state", name = "mint", parameter = "MintParams")]
-fn receive<A: HasActions>(
+#[receive(contract = "storable-contract", name = "mint", parameter = "MintParams")]
+fn receive_mint<A: HasActions, S: HasContractStateLL>(
     ctx: &impl HasReceiveContext,
-    state: &mut impl HasContractStateHL,
+    state: &mut State<S>,
+    allocator: &mut ContractStateHL<S>,
 ) -> ReceiveResult<A> {
     let params: MintParams = ctx.parameter_cursor().get()?;
 
-    let mut simple_map: StateMap<u8, u8, _> = state.get(Keys::SimpleMap).unwrap_abort()?;
-
-    // Inserts value in state
-    let _ = simple_map.insert(0, 1);
-    // Inserts value in state
-    let _ = simple_map.insert(1, 2);
-
-    let old_token_count: u64 = state.get(Keys::TokenCount).unwrap_abort()?;
-    state.insert(Keys::TokenCount, old_token_count + params.token_count);
-
-    let mut token_state: StateMap<Address, StateMap<TokenId, TokenCount, _>, _> =
-        state.get(Keys::TokenState).unwrap_abort()?;
-
-    token_state
+    // Just overwrite existing.
+    state
+        .token_state
         .entry(params.owner)?
         .and_try_modify::<_, Reject>(|owner_map| {
             owner_map
@@ -66,10 +137,23 @@ fn receive<A: HasActions>(
             Ok(())
         })?
         .or_insert({
-            let mut owner_map = state.new_map();
+            let mut owner_map = allocator.new_map();
             let _old_value = owner_map.insert(params.token_id, params.token_count);
             owner_map
         });
+
+    state.another_struct.a_set.insert(42);
+
+    // TODO: This won't be persisted
+    state.total_tokens += params.token_count as u64;
+
+    // But this should be.
+    // state.total_tokens_lsc.set(params.token_count as u64);
+    state
+        .total_tokens_p
+        .update(|old_count| *old_count += params.token_count as u64)
+        .unwrap_abort()
+        .unwrap_abort();
 
     Ok(A::accept())
 }
@@ -83,25 +167,10 @@ mod tests {
     fn test_init() {
         let ctx = InitContextTest::empty();
 
-        let mut state = ContractStateHL::open(ContractStateLLTest::new());
+        let state_ll = Rc::new(RefCell::new(ContractStateLLTest::new()));
+        let mut allocator = ContractStateHL::open(Rc::clone(&state_ll));
 
-        init(&ctx, &mut state).expect_report("Init failed");
-
-        // This doesn't check that the map type is correct, only that a map has been
-        // created with the given key and that it is empty.
-        claim!(state
-            .get::<_, StateMap<u8, u8, _>>(Keys::SimpleMap)
-            .unwrap_abort()
-            .unwrap_abort()
-            .is_empty());
-
-        claim!(state
-            .get::<_, StateMap<Address, StateMap<TokenId, TokenCount, _>, _>>(Keys::TokenState)
-            .unwrap_abort()
-            .unwrap_abort()
-            .is_empty());
-
-        claim_eq!(state.get(Keys::TokenCount), Some(Ok(0u64)));
+        let _state = init(&ctx, &mut allocator).expect_report("Init failed");
     }
 
     #[concordium_test]
@@ -117,29 +186,43 @@ mod tests {
         });
         ctx.set_parameter(&parameter_bytes);
 
-        let mut state = ContractStateHL::open(ContractStateLLTest::new());
+        let state_ll = Rc::new(RefCell::new(ContractStateLLTest::new()));
+        let mut allocator = ContractStateHL::open(Rc::clone(&state_ll));
 
         // Set up initial state contents via init.
-        init(&InitContextTest::empty(), &mut state).expect_report("Init failed");
+        let state_to_store =
+            init(&InitContextTest::empty(), &mut allocator).expect_report("Init failed");
+        state_to_store.store(&[], Rc::clone(&state_ll));
+
+        // Then load the state, as it happens when calling receive.
+        let mut state_for_rcv =
+            State::load(&[], Rc::clone(&state_ll)).expect_report("Could not load all of state.");
 
         // Invoke receive.
-        claim_eq!(receive(&ctx, &mut state), Ok(ActionsTree::accept()));
+        claim_eq!(
+            receive_mint(&ctx, &mut state_for_rcv, &mut allocator),
+            Ok(ActionsTree::accept())
+        );
+
+        // Reload the state to ensure that it was actually persisted (and not just
+        // altered in memory).
+        let mut state_reloaded =
+            State::load(&[], state_ll).expect_report("Could not load all of state.");
 
         // Token count should be 100.
-        claim_eq!(state.get(Keys::TokenCount), Some(Ok(expected_token_count)));
-
-        // Simple map should have (0, 1) and (1, 2).
-        let simple_map: StateMap<u8, u8, _> =
-            state.get(Keys::SimpleMap).unwrap_abort().unwrap_abort();
-        claim_eq!(simple_map.get(0), Some(Ok(1)));
-        claim_eq!(simple_map.get(1), Some(Ok(2)));
-
-        // TokenState should have (owner, (10, 100))
-        let token_state_map: StateMap<Address, StateMap<TokenId, TokenAmount, _>, _> =
-            state.get(Keys::TokenState).unwrap_abort().unwrap_abort();
         claim_eq!(
-            token_state_map.get(owner).unwrap_abort().unwrap_abort().get(token_id),
+            state_reloaded.token_state.get(owner).unwrap_abort().unwrap_abort().get(token_id),
             Some(Ok(expected_token_count))
-        )
+        );
+
+        let mut a_set_iter = state_reloaded.another_struct.a_set.iter();
+        claim_eq!(a_set_iter.next(), Some(Ok(42)));
+        claim_eq!(a_set_iter.next(), None);
+
+        claim_eq!(state_reloaded.total_tokens, 0); // The bare u64 is not persisted.
+        claim_eq!(state_reloaded.total_tokens_p.get(), Some(Ok(&100))); // The
+                                                                        // Persisted<u64>
+                                                                        // is
+                                                                        // persisted.
     }
 }
