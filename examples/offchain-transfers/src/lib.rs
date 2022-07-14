963d7c9f642663bd410bb090f37f6da59555e5a8
Christian Matt <cm@concordium.com>
2022-07-14 11:23:02 +0200

    Finished overall description.

--- examples/offchain-transfers/src/lib.rs
+++ examples/offchain-transfers/src/lib.rs
@@ -7,13 +7,17 @@
  * # Description
  * This contract implements a simple settlement mechanism for off-chain payments. It is an example of so-called "rollups" since it allows to roll multiple off-chain transaction up into a single on-chain settlement transaction (and thereby save transaction fees).
  * The intended use of the contract is as follows:
- *  * The smart contract is initialized by appointing a "judge" and a "validator", and setting a "time to finality" duration.
+ *  * The smart contract is initialized with [contract_init] by appointing a "judge" and a "validator", and setting a "time to finality" duration.
  *  * Users deposit a collateral to the smart contract using the [contract_receive_deposit] function. This adds the deposited amount to the available balance in the balance sheet of the smart contract.
  *  * Afterwards, users can transact off-chain using their deposited collateral as balance.
- *  * Once users are done with their off-chain transactions, the validator can settle the transactions by adding a settlement to the chain using [contract_receive_add_settlement].
+ *  * Once users are done with their off-chain transactions, the validator can settle the transactions by adding a settlement to the chain using [contract_receive_add_settlement]. A settlement consists of two `Vec`s of  addresses and amounts specifying which addresses have to pay which amounts and which addresses receive which amounts, respectively. Adding settlements can only be added by the validator to prevent DoS attacks.
+ *  * After a settlement, users can already (optimistically) use the updated balances from that settlement off-chain and in future settlements. Withdrawing received amounts, however, is only possible after the settlement was finalized.
+ *  * If users object to a published settlement, they can off-chain complain to the judge. If the judge deems a settlement invalid before it has been finalized, the judge can veto it using [contract_receive_veto].
+ *  * Settlements that have not been vetoed for the "time to finality" duration become finalized and cannot be reverted anymore.
+ *  * The function [contract_receive_execute_settlements] executes all finalized settlements and updates the balance sheet accordingly. Everyone can call this function periodically.
+ *  * Users can withdraw funds from the smart contract using [contract_receive_withdraw]. The maximal allowed amount to withdraw corresponds to the worst-case amount that is guaranteed to be available no matter which outstanding settlements are vetoed.
  * 
  * # Limitations
- * 
  *  * The `settlement_limit` in [ContractConfig] needs to be set such that both `contract_receive_veto` and `contract_receive_execute_settlements` can run on a full settlement queue given the energy limit of a single block. 
  *  * The data structures have not been optimized for deployment. In particular, the use of `Vec` in the smart contract state can degrade performance.
  * 
@@ -27,7 +31,7 @@ use std::convert::TryInto;
 /// Unique identifier for settlements
 type SettlementID = u64;
 
-/// A tuple describing either a sender or reicer in a transfer
+/// A tuple describing either a sender or receiver with an amount in a transfer
 #[derive(Clone, Serialize, SchemaType)]
 pub struct AddressAmount {
     /// The sender or receiver
