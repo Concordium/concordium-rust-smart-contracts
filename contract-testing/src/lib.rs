450f939464d16e634564f48dcb7275110308ea5a
Kasper Dissing Bargsteen <kb@concordium.com>
2022-11-30 17:20:17 +0100

    Implement bare-bones contract initialization

--- contract-testing/src/lib.rs
+++ contract-testing/src/lib.rs
@@ -1,14 +1,19 @@
+use anyhow::anyhow;
 use concordium_base::base::{Energy, ExchangeRate};
 use concordium_contracts_common::*;
 use sha2::{Digest, Sha256};
 use std::{
     collections::{
         btree_map::Entry::{Occupied, Vacant},
-        BTreeMap,
+        BTreeMap, LinkedList,
     },
-    path::{Path, PathBuf},
+    path::Path,
 };
-use wasm_chain_integration::v1::ConcordiumAllowedImports;
+use wasm_chain_integration::{
+    v0,
+    v1::{self, ConcordiumAllowedImports, ReturnValue},
+    ExecResult, InterpreterEnergy,
+};
 use wasm_transform::artifact;
 
 pub struct Chain {
@@ -16,27 +21,37 @@ pub struct Chain {
     /// An error is thrown if this is `None` and the contract tries to
     /// access it.
     slot_time:            Option<SlotTime>,
-    /// Energy per microCCD.
-    energy_per_micro_ccd: ExchangeRate,
+    /// MicroCCD per Energy.
+    micro_ccd_per_energy: ExchangeRate,
     /// Accounts and info about them.
     accounts:             BTreeMap<AccountAddress, AccountInfo>,
     /// Smart contract modules.
     modules: BTreeMap<
         ModuleReference,
-        artifact::Artifact<
-            wasm_transform::artifact::ArtifactNamedImport,
-            artifact::CompiledFunction,
-        >,
+        artifact::Artifact<v1::ProcessedImports, artifact::CompiledFunction>,
     >,
+    /// Smart contract instances.
+    contracts:            BTreeMap<ContractAddress, v1::trie::MutableState>,
+    /// Next contract index to use when creating a new instance.
+    next_contract_index:  u64,
 }
 
 impl Chain {
-    pub fn new(slot_time: SlotTime, energy_per_micro_ccd: ExchangeRate) -> Self {
+    pub fn new_with_time(slot_time: SlotTime, energy_per_micro_ccd: ExchangeRate) -> Self {
         Self {
             slot_time: Some(slot_time),
-            energy_per_micro_ccd,
-            accounts: BTreeMap::new(),
-            modules: BTreeMap::new(),
+            ..Self::new(energy_per_micro_ccd)
+        }
+    }
+
+    pub fn new(energy_per_micro_ccd: ExchangeRate) -> Self {
+        Self {
+            slot_time:            None,
+            micro_ccd_per_energy: energy_per_micro_ccd,
+            accounts:             BTreeMap::new(),
+            modules:              BTreeMap::new(),
+            contracts:            BTreeMap::new(),
+            next_contract_index:  0,
         }
     }
 
@@ -49,10 +64,7 @@ impl Chain {
         let module = std::fs::read(module_path)?;
 
         // Deserialize as wasm module (artifact)
-        let artifact = wasm_transform::utils::instantiate_with_metering::<
-            wasm_transform::artifact::ArtifactNamedImport,
-            _,
-        >(
+        let artifact = wasm_transform::utils::instantiate_with_metering::<v1::ProcessedImports, _>(
             &ConcordiumAllowedImports {
                 support_upgrade: true,
             },
@@ -93,14 +105,147 @@ impl Chain {
 
     pub fn contract_init(
         &mut self,
-        _sender: AccountAddress,
-        _module: ModuleReference,
-        _contract_name: ContractName,
-        _parameter: ContractParameter,
-        _amount: Amount,
-        _energy: Energy,
-    ) -> Result<SuccessfulContractInit, FailedContractInteraction> {
-        todo!()
+        sender: AccountAddress,
+        module: ModuleReference,
+        contract_name: ContractName,
+        parameter: ContractParameter,
+        amount: Amount,
+        energy_limit: Energy,
+    ) -> Result<SuccessfulContractInit, ContractInitError> {
+        // Lookup artifact
+        let artifact = self
+            .modules
+            .get(&module)
+            .ok_or_else(|| ContractInitError::ModuleDoesNotExist)?;
+        // Get the account and check that it has sufficient balance to pay for the
+        // energy.
+        let account_info = self
+            .accounts
+            .get(&sender)
+            .ok_or_else(|| ContractInitError::AccountDoesNotExist)?;
+        if account_info.balance < self.calculate_energy_cost(energy_limit) {
+            return Err(ContractInitError::InsufficientFunds);
+        }
+        // Construct the context.
+        let init_ctx = InitContextOpt {
+            metadata:        ChainMetadataOpt {
+                slot_time: self.slot_time,
+            },
+            init_origin:     sender,
+            sender_policies: account_info.policies.clone(),
+        };
+        // Initialize contract
+        let loader = v1::trie::Loader::new(&[][..]);
+        let res = v1::invoke_init(
+            artifact,
+            init_ctx,
+            v1::InitInvocation {
+                amount,
+                init_name: contract_name.get_chain_name(),
+                parameter: &parameter.0,
+                energy: InterpreterEnergy {
+                    // TODO: Why do we have two separate energy types?
+                    energy: energy_limit.energy,
+                },
+            },
+            false,
+            loader,
+        )
+        .map_err(|e| ContractInitError::StringInitError(e.to_string()))?;
+        // Charge account for cost
+        let (res, transaction_fee) = match res {
+            v1::InitResult::Success {
+                logs,
+                return_value: _, /* Ignore return value for now, since our tools do not support
+                                  * it for inits, currently. */
+                remaining_energy,
+                state,
+            } => {
+                let contract_address = self.create_contract_address();
+                let energy_used = Energy::from(energy_limit.energy - remaining_energy);
+                let transaction_fee = self.calculate_energy_cost(energy_used);
+
+                // Save the state
+                self.contracts.insert(contract_address, state); // Ignore the return value since we created a new contract address for this.
+
+                (
+                    Ok(SuccessfulContractInit {
+                        contract_address,
+                        logs,
+                        energy_used,
+                        transaction_fee,
+                    }),
+                    transaction_fee,
+                )
+            }
+            v1::InitResult::Reject {
+                reason,
+                return_value,
+                remaining_energy,
+            } => {
+                let energy_used = Energy::from(energy_limit.energy - remaining_energy);
+                let transaction_fee = self.calculate_energy_cost(energy_used);
+                (
+                    Err(ContractInitError::ValidChainError(
+                        FailedContractInteraction::Reject {
+                            reason,
+                            return_value,
+                            energy_used,
+                            transaction_fee,
+                            logs: v0::Logs {
+                                // TODO: Get logs on failures.
+                                logs: LinkedList::new(),
+                            },
+                        },
+                    )),
+                    transaction_fee,
+                )
+            }
+            v1::InitResult::Trap {
+                error,
+                remaining_energy,
+            } => {
+                let energy_used = Energy::from(energy_limit.energy - remaining_energy);
+                let transaction_fee = self.calculate_energy_cost(energy_used);
+                (
+                    Err(ContractInitError::ValidChainError(
+                        FailedContractInteraction::Trap {
+                            error,
+                            energy_used,
+                            transaction_fee,
+                            logs: v0::Logs {
+                                // TODO: Get logs on failures.
+                                logs: LinkedList::new(),
+                            },
+                        },
+                    )),
+                    transaction_fee,
+                )
+            }
+            v1::InitResult::OutOfEnergy => {
+                let transaction_fee = self.calculate_energy_cost(energy_limit);
+                (
+                    Err(ContractInitError::ValidChainError(
+                        FailedContractInteraction::OutOFEnergy {
+                            energy_used: energy_limit,
+                            transaction_fee,
+                            logs: v0::Logs {
+                                // TODO: Get logs on failures.
+                                logs: LinkedList::new(),
+                            },
+                        },
+                    )),
+                    transaction_fee,
+                )
+            }
+        };
+        // Charge the account.
+        // We have to get the account info again because of the borrow checker.
+        self.accounts
+            .get_mut(&sender)
+            .ok_or_else(|| ContractInitError::AccountDoesNotExist)?
+            .balance -= transaction_fee;
+        res
     }
 
     /// Can we get the return value here?
@@ -138,12 +283,17 @@ impl Chain {
     /// Creates a contract address with an index one above the highest
     /// currently used. Next call to `contract_init` will skip this
     /// address.
-    pub fn create_contract_address(&mut self) -> ContractAddress { todo!() }
+    pub fn create_contract_address(&mut self) -> ContractAddress {
+        let index = self.next_contract_index;
+        let subindex = 0;
+        self.next_contract_index += 1;
+        ContractAddress::new(index, subindex)
+    }
 
     pub fn set_slot_time(&mut self, slot_time: SlotTime) { self.slot_time = Some(slot_time); }
 
     pub fn set_energy_per_micro_ccd(&mut self, energy_per_micro_ccd: ExchangeRate) {
-        self.energy_per_micro_ccd = energy_per_micro_ccd;
+        self.micro_ccd_per_energy = energy_per_micro_ccd;
     }
 
     /// Returns the balance of an account if it exists.
@@ -153,9 +303,61 @@ impl Chain {
 
     pub fn calculate_energy_cost(&self, energy: Energy) -> Amount {
         Amount::from_micro_ccd(
-            energy.energy * self.energy_per_micro_ccd.numerator()
-                / self.energy_per_micro_ccd.denominator(),
+            energy.energy * self.micro_ccd_per_energy.numerator()
+                / self.micro_ccd_per_energy.denominator(),
         )
+    }
+}
+
+/// A chain metadata with an optional field.
+/// Used when simulating contracts to allow the user to only specify the
+/// necessary context fields.
+/// The default value is `None` for all `Option` fields.
+pub(crate) struct ChainMetadataOpt {
+    slot_time: Option<SlotTime>,
+}
+
+impl v0::HasChainMetadata for ChainMetadataOpt {
+    fn slot_time(&self) -> ExecResult<SlotTime> {
+        unwrap_ctx_field(self.slot_time, "metadata.slotTime")
+    }
+}
+
+/// An init context with optional fields.
+/// Used when simulating contracts to allow the user to only specify the
+/// context fields used by the contract.
+/// The default value is `None` for all `Option` fields and the default of
+/// `ChainMetadataOpt` for `metadata`.
+pub(crate) struct InitContextOpt {
+    metadata:        ChainMetadataOpt,
+    init_origin:     AccountAddress,
+    sender_policies: Option<v0::OwnedPolicyBytes>,
+}
+
+impl v0::HasInitContext for InitContextOpt {
+    type MetadataType = ChainMetadataOpt;
+
+    fn metadata(&self) -> &Self::MetadataType { &self.metadata }
+
+    fn init_origin(&self) -> ExecResult<&AccountAddress> { Ok(&self.init_origin) }
+
+    fn sender_policies(&self) -> ExecResult<&[u8]> {
+        unwrap_ctx_field(
+            self.sender_policies.as_ref().map(Vec::as_ref),
+            "senderPolicies",
+        )
+    }
+}
+
+// Error handling when unwrapping.
+fn unwrap_ctx_field<A>(opt: Option<A>, name: &str) -> ExecResult<A> {
+    match opt {
+        Some(v) => Ok(v),
+        None => Err(anyhow!(
+            "Missing field '{}' in the context. Make sure to provide all the fields that the \
+             contract uses.",
+            name,
+        )),
     }
 }
 
@@ -163,7 +365,7 @@ pub struct AccountInfo {
     /// The account balance. TODO: Do we need the three types of balances?
     pub balance: Amount,
     /// Optional test policies.
-    policies:    Option<Policies>,
+    policies:    Option<v0::OwnedPolicyBytes>,
 }
 
 impl AccountInfo {
@@ -175,16 +377,48 @@ impl AccountInfo {
     }
 }
 
+pub enum ContractInitError {
+    /// Initialization failed for a reason that also exists on the chain.
+    ValidChainError(FailedContractInteraction),
+    /// TODO: Can we get a better error than the anyhow?
+    StringInitError(String),
+    /// Module has not been deployed in test environment.
+    ModuleDoesNotExist,
+    /// Account has not been created in test environment.
+    AccountDoesNotExist,
+    /// The account does not have enough funds to pay for the energy.
+    InsufficientFunds,
+}
+
 #[derive(Debug)]
-pub struct FailedContractInteraction {
-    /// Energy spent.
-    pub energy: Energy,
-    /// Error returned.
-    pub error:  ContractError,
-    /// Events emitted before the interaction failed. Events from failed
-    /// updates are not stored on the chain, but can be useful for
-    /// debugging.
-    pub events: Vec<Event>,
+pub enum FailedContractInteraction {
+    Reject {
+        reason:          i32,
+        return_value:    ReturnValue,
+        energy_used:     Energy,
+        transaction_fee: Amount,
+        /// Logs emitted before the interaction failed. Logs from failed
+        /// updates are not stored on the chain, but can be useful for
+        /// debugging.
+        logs:            v0::Logs,
+    },
+    Trap {
+        error:           anyhow::Error,
+        energy_used:     Energy,
+        transaction_fee: Amount,
+        /// Logs emitted before the interaction failed. Logs from failed
+        /// updates are not stored on the chain, but can be useful for
+        /// debugging.
+        logs:            v0::Logs,
+    },
+    OutOFEnergy {
+        energy_used:     Energy,
+        transaction_fee: Amount,
+        /// Logs emitted before the interaction failed. Logs from failed
+        /// updates are not stored on the chain, but can be useful for
+        /// debugging.
+        logs:            v0::Logs,
+    },
 }
 
 #[derive(Debug)]
@@ -255,19 +489,17 @@ pub struct SuccessfulModuleDeployment {
     pub transaction_fee:  Amount,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug)]
 pub struct SuccessfulContractInit {
     /// The address of the new instance.
     pub contract_address: ContractAddress,
-    /// Events produced during initialization.
-    pub events:           Vec<Event>,
+    /// Logs produced during initialization.
+    pub logs:             v0::Logs,
     /// Energy used.
-    pub energy:           Energy,
+    pub energy_used:      Energy,
     /// Cost of transaction.
     pub transaction_fee:  Amount,
 }
-
-pub struct Policies;
 
 pub struct ContractReturnValue(Vec<u8>);
 
@@ -281,7 +513,7 @@ impl ContractReturnValue {
     pub fn deserial<T: Deserial>(&self) -> Result<T, ParsingError> { todo!() }
 }
 
-pub struct ContractParameter(Vec<u8>);
+pub struct ContractParameter(pub Vec<u8>);
 
 impl ContractParameter {
     pub fn empty() -> Self { Self(Vec::new()) }
@@ -298,16 +530,13 @@ mod tests {
 
     #[test]
     fn deploying_valid_module_works() {
-        let mut chain = Chain::new(
-            Timestamp::from_timestamp_millis(0),
-            ExchangeRate::new_unchecked(10, 1),
-        );
+        let mut chain = Chain::new(ExchangeRate::new_unchecked(2404, 1));
         let acc_0 = AccountAddress([0; 32]);
         let initial_balance = Amount::from_ccd(10000);
         chain.create_account(acc_0, AccountInfo::new(initial_balance));
 
         let res = chain
-            .module_deploy(acc_0, "icecream/icecream.wasm.v1")
+            .module_deploy(acc_0, "icecream/icecream.wasm.v1") // TODO: Add wasm files to the repo for tests.
             .expect("Deploying valid module should work");
 
         assert_eq!(chain.accounts.len(), 1);
