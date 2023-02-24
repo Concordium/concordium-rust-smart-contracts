261bea8655ebc54138e40ac8a91a1c975760b733
Kasper Dissing Bargsteen <kb@concordium.com>
2023-02-24 15:44:24 +0100

    Add iterator and recorder integration tests

---
+++ contract-testing/tests/iterator.rs
@@ -0,0 +1,55 @@
+//! This module tests calling a contract which makes use of an iterator.
+//! The checks are being performed in the contract itself so if invoking the
+//! contract completes successfully then this implies that the tests have done
+//! so as well. Note. as per above no checks are being performed in this file
+//! wrt. the state etc. after execution etc.
+
+use concordium_smart_contract_testing::*;
+
+const WASM_TEST_FOLDER: &str = "../../concordium-node/concordium-consensus/testdata/contracts/v1";
+const ACC_0: AccountAddress = AccountAddress([0; 32]);
+
+#[test]
+fn test_iterator() {
+    let mut chain = Chain::new();
+    let initial_balance = Amount::from_ccd(1000000);
+    chain.create_account(ACC_0, Account::new(initial_balance));
+
+    let res_deploy = chain
+        .module_deploy_wasm_v1(ACC_0, format!("{}/iterator.wasm", WASM_TEST_FOLDER))
+        .expect("Deploying valid module should work");
+
+    let res_init = chain
+        .contract_init(
+            ACC_0,
+            res_deploy.module_reference,
+            ContractName::new_unchecked("init_iterator"),
+            OwnedParameter::empty(),
+            Amount::zero(),
+            Energy::from(10000),
+        )
+        .expect("Initializing valid contract should work");
+
+    chain
+        .contract_update(
+            ACC_0,
+            Address::Account(ACC_0),
+            res_init.contract_address,
+            EntrypointName::new_unchecked("iteratetest"),
+            OwnedParameter::empty(),
+            Amount::zero(),
+            Energy::from(10000),
+        )
+        .expect("Should succeed");
+    chain
+        .contract_update(
+            ACC_0,
+            Address::Account(ACC_0),
+            res_init.contract_address,
+            EntrypointName::new_unchecked("lockingtest"),
+            OwnedParameter::empty(),
+            Amount::zero(),
+            Energy::from(10000),
+        )
+        .expect("Should succeed.");
+}
---
+++ contract-testing/tests/recorder.rs
@@ -0,0 +1,60 @@
+//! This module tests basic V1 state operations with the recorder contract.
+
+use concordium_smart_contract_testing::*;
+
+const WASM_TEST_FOLDER: &str = "../../concordium-node/concordium-consensus/testdata/contracts/v1";
+const ACC_0: AccountAddress = AccountAddress([0; 32]);
+
+#[test]
+fn test_recorder() {
+    let mut chain = Chain::new();
+    let initial_balance = Amount::from_ccd(1000000);
+    chain.create_account(ACC_0, Account::new(initial_balance));
+
+    let res_deploy = chain
+        .module_deploy_wasm_v1(
+            ACC_0,
+            format!("{}/record-parameters.wasm", WASM_TEST_FOLDER),
+        )
+        .expect("Deploying valid module should work");
+
+    let res_init = chain
+        .contract_init(
+            ACC_0,
+            res_deploy.module_reference,
+            ContractName::new_unchecked("init_recorder"),
+            OwnedParameter::empty(),
+            Amount::zero(),
+            Energy::from(10000),
+        )
+        .expect("Initializing valid contract should work");
+
+    chain
+        .contract_update(
+            ACC_0,
+            Address::Account(ACC_0),
+            res_init.contract_address,
+            EntrypointName::new_unchecked("record_u64"),
+            OwnedParameter::new(&20u64),
+            Amount::zero(),
+            Energy::from(100000),
+        )
+        .expect("Update failed");
+    chain
+        .contract_update(
+            ACC_0,
+            Address::Account(ACC_0),
+            res_init.contract_address,
+            EntrypointName::new_unchecked("record_u64"),
+            OwnedParameter::new(&40u64),
+            Amount::zero(),
+            Energy::from(100000),
+        )
+        .expect("Update failed");
+    // Assert that all 60 values were inserted in the state.
+    for key in 0..60u64 {
+        assert!(chain
+            .contract_state_lookup(res_init.contract_address, &u64::to_le_bytes(key))
+            .is_some());
+    }
+}
