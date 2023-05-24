2d8c94f4ffa5f5226393dea7ce6024015afa05ca
Emil Lai <el@concordium.com>
2023-05-24 10:49:05 +0200

    Call `slot_time` directly.

--- concordium-std/src/test_infrastructure.rs
+++ concordium-std/src/test_infrastructure.rs
@@ -449,7 +449,7 @@ fn unwrap_ctx_field<A>(opt: Option<A>, name: &str) -> A {
 impl HasChainMetadata for TestChainMeta {
     fn slot_time(&self) -> SlotTime { unwrap_ctx_field(self.slot_time, "metadata.slot_time") }
 
-    fn block_time(&self) -> Timestamp { unwrap_ctx_field(self.slot_time, "metadata.slot_time") }
+    fn block_time(&self) -> Timestamp { self.slot_time() }
 }
 
 pub struct TestIterator {
