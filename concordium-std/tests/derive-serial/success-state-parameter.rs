055aeb9d9f0c4c802cf5b89799f702e5b9a2f3e4
Emil Holm Gjørup <eg@concordium.com>
2023-05-26 10:16:48 +0200

    Add derive test for Serial with state_parameter

---
+++ concordium-std/tests/derive-serial/success-state-parameter.rs
@@ -0,0 +1,23 @@
+//! Ensure `derive(Serial)` generates code successfully for the
+//! simplest case when `#[concordium(state_parameter)]` is set.
+use concordium_std::*;
+
+#[derive(Serial, DeserialWithState, Deletable, StateClone)]
+#[concordium(state_parameter = "S")]
+struct State<S> {
+    map: StateMap<u8, u16, S>,
+    set: StateSet<u32, S>,
+}
+
+#[init(contract = "test")]
+fn contract_init<S: HasStateApi>(
+    _ctx: &impl HasInitContext,
+    state_builder: &mut StateBuilder<S>,
+) -> InitResult<State<S>> {
+    Ok(State {
+        map: state_builder.new_map(),
+        set: state_builder.new_set(),
+    })
+}
+
+fn main() {}
