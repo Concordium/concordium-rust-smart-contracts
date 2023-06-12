87754bf9bc481c0957e93eb128b283b95da1d2ac
Emil Holm Gjørup <eg@concordium.com>
2023-06-12 08:57:03 +0200

    Add derive schematype test with size_length field attribute

--- concordium-std/tests/derive-schema-type/success-transparent.rs
+++ concordium-std/tests/derive-schema-type/success-transparent.rs
@@ -1,16 +1,26 @@
 /// Test ensuring derivation works for newtype wrappers with transparent
 use concordium_std::*;
 
+/// Simple struct
 #[derive(SchemaType)]
 #[concordium(transparent)]
 struct MyStruct {
     field: u32,
 }
 
+/// Nested struct
 #[derive(SchemaType)]
 #[concordium(transparent)]
 struct MyOtherStruct {
     field: MyStruct,
+}
+
+/// With field attributes
+#[derive(SchemaType)]
+#[concordium(transparent)]
+struct MyCollection {
+    #[concordium(size_length = 1)]
+    field: Vec<u32>,
 }
 
 fn main() {
@@ -22,5 +32,13 @@ fn main() {
     {
         let schema_type = <MyOtherStruct as schema::SchemaType>::get_type();
         assert_eq!(schema_type, schema::Type::U32);
+    }
+
+    {
+        let schema_type = <MyCollection as schema::SchemaType>::get_type();
+        assert_eq!(
+            schema_type,
+            schema::Type::List(schema::SizeLength::U8, Box::new(schema::Type::U32))
+        );
     }
 }
