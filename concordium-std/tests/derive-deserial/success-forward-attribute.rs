77878be6afccb37b4faa3ecab429180ae5470ac9
Emil Holm Gjørup <eg@concordium.com>
2023-07-10 15:08:34 +0200

    Add success tests for predefined forward tags

--- concordium-std/tests/derive-deserial/success-forward-attribute.rs
+++ concordium-std/tests/derive-deserial/success-forward-attribute.rs
@@ -23,6 +23,33 @@ enum Inner {
     Beta(u16),
 }
 
+// Example using predefined forwarded tags.
+
+#[derive(Debug, Deserial, PartialEq, Eq)]
+#[concordium(repr(u8))]
+enum Event {
+    A {
+        field: u32,
+    },
+    #[concordium(forward = cis2_events)]
+    B(Cis2Event),
+}
+
+#[derive(Debug, Deserial, PartialEq, Eq)]
+#[concordium(repr(u8))]
+enum Cis2Event {
+    #[concordium(tag = 255)]
+    Transfer,
+    #[concordium(tag = 254)]
+    Mint,
+    #[concordium(tag = 253)]
+    Burn,
+    #[concordium(tag = 252)]
+    UpdateOperator,
+    #[concordium(tag = 251)]
+    TokenMetadata,
+}
+
 fn main() {
     {
         let bytes = [0, 42, 0, 0, 0];
@@ -50,5 +77,11 @@ fn main() {
         let bytes = [6, 8, 0];
         let value: Count = from_bytes(&bytes).unwrap();
         assert_eq!(Count::Two(Inner::Beta(8)), value);
+    }
+
+    {
+        let bytes = [254];
+        let value: Event = from_bytes(&bytes).unwrap();
+        assert_eq!(Event::B(Cis2Event::Mint), value);
     }
 }
--- concordium-std/tests/derive-serial/success-forward-attribute.rs
+++ concordium-std/tests/derive-serial/success-forward-attribute.rs
@@ -23,6 +23,33 @@ enum Inner {
     Beta(u16),
 }
 
+// Example using predefined forwarded tags.
+
+#[derive(Serial)]
+#[concordium(repr(u8))]
+enum Event {
+    A {
+        field: u32,
+    },
+    #[concordium(forward = cis2_events)]
+    B(Cis2Event),
+}
+
+#[derive(Serial)]
+#[concordium(repr(u8))]
+enum Cis2Event {
+    #[concordium(tag = 255)]
+    Transfer,
+    #[concordium(tag = 254)]
+    Mint,
+    #[concordium(tag = 253)]
+    Burn,
+    #[concordium(tag = 252)]
+    UpdateOperator,
+    #[concordium(tag = 251)]
+    TokenMetadata,
+}
+
 fn main() {
     {
         let value = Count::One {
@@ -43,5 +70,11 @@ fn main() {
         let value = Count::Two(Inner::Beta(8));
         let bytes = to_bytes(&value);
         assert_eq!(bytes, vec![6, 8, 0]);
+    }
+
+    {
+        let value = Event::B(Cis2Event::Transfer);
+        let bytes = to_bytes(&value);
+        assert_eq!(bytes, vec![255]);
     }
 }
