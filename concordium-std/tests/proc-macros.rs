66792033c0348a6553bbea03561153e27d7017c7
Target-san <target.san@gmail.com>
2023-08-10 12:33:11 +0300

    `derives` -> `proc_macros`, intended to test not only derives in future

--- concordium-std/tests/proc-macros.rs
+++ concordium-std/tests/proc-macros.rs
@@ -2,34 +2,34 @@
 //! `concordium-std-derive` package. Test cases presented here check successful
 //! (or failed) compilation for the code which uses macros, not its functioning.
 #[test]
-fn deserial_with_state() {
+fn derive_deserial_with_state() {
     let t = trybuild::TestCases::new();
     t.pass("tests/derive-deserial-with-state/success-*.rs");
     t.compile_fail("tests/derive-deserial-with-state/fail-*.rs");
 }
 
 #[test]
-fn deletable() {
+fn derive_deletable() {
     let t = trybuild::TestCases::new();
     t.pass("tests/derive-deletable/success-*.rs");
 }
 
 #[test]
-fn serial() {
+fn derive_serial() {
     let t = trybuild::TestCases::new();
     t.pass("tests/derive-serial/success-*.rs");
     t.compile_fail("tests/derive-serial/fail-*.rs");
 }
 
 #[test]
-fn deserial() {
+fn derive_deserial() {
     let t = trybuild::TestCases::new();
     t.pass("tests/derive-deserial/success-*.rs");
     t.compile_fail("tests/derive-deserial/fail-*.rs");
 }
 
 #[test]
-fn schema_type() {
+fn derive_schema_type() {
     let t = trybuild::TestCases::new();
     t.pass("tests/derive-schema-type/success-*.rs");
     t.compile_fail("tests/derive-schema-type/fail-*.rs");
