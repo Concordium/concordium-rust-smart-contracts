888344aa1b2f2d01d65bd88004043c69073f2e1a
Doris Benda <DOBEN@users.noreply.github.com>
2023-11-28 21:13:49 +0300

    Add comments

--- examples/cis2-multi/tests/tests.rs
+++ examples/cis2-multi/tests/tests.rs
@@ -772,7 +772,7 @@ fn initialize_chain_and_contract() -> (Chain, AccountKeys, ContractAddress) {
             mod_ref:   deployment.module_reference,
             init_name: OwnedContractName::new_unchecked("init_cis2_multi".to_string()),
             param:     OwnedParameter::from_serial(&TokenAmountU64(100))
-                .expect("UpdateOperator params"),
+                .expect("Init params"),
         })
         .expect("Initialize contract");
 
