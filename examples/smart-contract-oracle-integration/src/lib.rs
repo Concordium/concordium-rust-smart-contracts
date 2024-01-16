07209c6d5e51afe3722830f99b45755c25d5d428
Doris Benda <DOBEN@users.noreply.github.com>
2024-01-16 23:01:30 +0100

    Add test cases

--- examples/smart-contract-oracle-integration/src/lib.rs
+++ examples/smart-contract-oracle-integration/src/lib.rs
@@ -29,8 +29,54 @@
 
 use concordium_std::*;
 
+#[derive(Debug, Serial, SchemaType)]
+pub struct InitParamsUmbrellaFeeds {
+    pub registry:            ContractAddress,
+    pub required_signatures: u16,
+    pub staking_bank:        ContractAddress,
+    pub decimals:            u8,
+}
+
+#[derive(Serialize, SchemaType, Copy, Clone, Debug, PartialOrd, Ord, PartialEq, Eq)]
+pub struct PriceData {
+    /// This is a placeholder, that can be used for some additional data.
+    pub data:      u8,
+    /// The heartbeat specifies the interval in seconds that the price data will
+    /// be refreshed in case the price stays flat. ATTENTION: u64 is used
+    /// here instead of u24 (different from the original solidity smart
+    /// contracts).
+    pub heartbeat: u64,
+    /// It is the time the validators run consensus to decide on the price data.
+    pub timestamp: Timestamp,
+    /// The relative price.
+    pub price:     u128,
+}
+
+/// Part of the parameter type for the contract function `update`.
+/// Specifies the message that is signed.
+#[derive(SchemaType, Serialize, Clone)]
+pub struct Message {
+    /// The contract_address that the signature is intended for.
+    pub contract_address: ContractAddress,
+    /// A timestamp to make signatures expire.
+    pub timestamp:        Timestamp,
+    /// The price feed.
+    pub price_feed:       Vec<(String, PriceData)>,
+}
+
+/// The parameter type for the contract function `update` and
+/// `view_message_hash`. Takes a vector of signers and signatures, and the
+/// message that was signed.
+#[derive(Serialize, SchemaType)]
+pub struct UpdateParams {
+    /// Signers and signatures.
+    pub signers_and_signatures: Vec<(PublicKeyEd25519, SignatureEd25519)>,
+    /// Message that was signed.
+    pub message:                Message,
+}
+
 const UMBRELLA_REGISTRY_CONTRACT: ContractAddress = ContractAddress {
-    index: 99,
+    index:    7542,
     subindex: 0,
 };
 
@@ -49,7 +95,7 @@ struct State<S = StateApi> {
     ///
     umbrella_registry_contract: ContractAddress,
     ///
-    last_price_update: StateMap<String, u64, S>,
+    last_price_update:          StateMap<String, u64, S>,
 }
 
 /// Errors
@@ -66,23 +112,25 @@ pub enum CustomContractError {
 
 /// Mapping errors related to contract invocations to CustomContractError.
 impl<T> From<CallContractError<T>> for CustomContractError {
-    fn from(_cce: CallContractError<T>) -> Self {
-        Self::InvokeContractError
-    }
+    fn from(_cce: CallContractError<T>) -> Self { Self::InvokeContractError }
 }
 
 /// Init function that creates a new auction
-#[init(contract = "oracle")]
+#[init(contract = "smart_contract_oracle_integration")]
 fn init(_ctx: &InitContext, state_builder: &mut StateBuilder) -> InitResult<State> {
     Ok(State {
         umbrella_registry_contract: UMBRELLA_REGISTRY_CONTRACT,
-        last_price_update: state_builder.new_map(),
+        last_price_update:          state_builder.new_map(),
     })
 }
 
 /// View function that returns the content of the state
-#[receive(contract = "oracle", name = "view", return_value = "Vec<(String, u64)>")]
-fn view<'b>(_ctx: &ReceiveContext, host: &Host<State>) -> ReceiveResult<Vec<(String, u64)>> {
+#[receive(
+    contract = "smart_contract_oracle_integration",
+    name = "prices",
+    return_value = "Vec<(String, u64)>"
+)]
+fn prices<'b>(_ctx: &ReceiveContext, host: &Host<State>) -> ReceiveResult<Vec<(String, u64)>> {
     let prices: Vec<(String, u64)> =
         host.state().last_price_update.iter().map(|(a, b)| ((*a).clone(), *b)).collect();
     Ok(prices)
@@ -92,7 +140,7 @@ fn view<'b>(_ctx: &ReceiveContext, host: &Host<State>) -> ReceiveResult<Vec<(Str
 /// current balance of this smart contract) to the owner of the smart contract
 /// instance.
 #[receive(
-    contract = "oracle",
+    contract = "smart_contract_oracle_integration",
     name = "update_price",
     parameter = "String",
     error = "CustomContractError",
---
+++ examples/smart-contract-oracle-integration/tests/tests.rs
@@ -0,0 +1,179 @@
+//! Tests for the auction smart contract.
+use concordium_smart_contract_testing::*;
+use concordium_std::{PublicKeyEd25519, SignatureEd25519};
+
+use smart_contract_oracle_integration::*;
+
+/// The tests accounts.
+const ALICE: AccountAddress = AccountAddress([0; 32]);
+const ALICE_ADDR: Address = Address::Account(ALICE);
+
+const ACC_INITIAL_BALANCE: Amount = Amount::from_ccd(10000);
+const SIGNER: Signer = Signer::with_one_key();
+
+const DUMMY_SIGNATURE: SignatureEd25519 = SignatureEd25519([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+]);
+
+/// Test a sequence of bids and finalizations:
+#[test]
+fn test_multiple_scenarios() {
+    let (mut chain, initialization_umbrella_feeds, contract_address) =
+        initialize_chain_and_contract();
+
+    let key: String = String::from("CCD-USD");
+
+    let price_data = PriceData {
+        data:      7,
+        heartbeat: 12,
+        timestamp: Timestamp::from_timestamp_millis(9),
+        price:     4,
+    };
+
+    use ed25519_dalek::{Signer, SigningKey};
+
+    let rng = &mut rand::thread_rng();
+
+    // Construct message, verifying_key, and signature.
+    let signing_key = SigningKey::generate(rng);
+    let verifying_key = signing_key.verifying_key();
+
+    let mut param = UpdateParams {
+        signers_and_signatures: vec![(PublicKeyEd25519(verifying_key.to_bytes()), DUMMY_SIGNATURE)],
+        message:                Message {
+            contract_address: initialization_umbrella_feeds,
+            timestamp:        Timestamp::from_timestamp_millis(1000000000000),
+            price_feed:       vec![(key, price_data)],
+        },
+    };
+
+    // Get the message hash to be signed.
+    let invoke = chain
+        .contract_invoke(ALICE, ALICE_ADDR, Energy::from(10000), UpdateContractPayload {
+            amount:       Amount::zero(),
+            address:      initialization_umbrella_feeds,
+            receive_name: OwnedReceiveName::new_unchecked(
+                "umbrella_feeds.viewMessageHash".to_string(),
+            ),
+            message:      OwnedParameter::from_serial(&param)
+                .expect("Should be a valid inut parameter"),
+        })
+        .expect("Should be able to query viewMessageHash");
+
+    let signature = signing_key.sign(&invoke.return_value);
+
+    param.signers_and_signatures =
+        vec![(PublicKeyEd25519(verifying_key.to_bytes()), SignatureEd25519(signature.to_bytes()))];
+
+    let _update_1 = chain
+        .contract_update(
+            concordium_smart_contract_testing::Signer::with_one_key(),
+            ALICE,
+            Address::Account(ALICE),
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount:       Amount::zero(),
+                address:      contract_address,
+                receive_name: OwnedReceiveName::new_unchecked(
+                    "smart_contract_oracle_integration.update_price".to_string(),
+                ),
+                message:      OwnedParameter::from_serial(&String::from("CCD-USD"))
+                    .expect("Serialize parameter"),
+            },
+        )
+        .expect("Alice successfully bids 1 CCD");
+
+    // Invoke the view entry point and check that the item was added.
+    let invoke = chain
+        .contract_invoke(ALICE, ALICE_ADDR, Energy::from(10000), UpdateContractPayload {
+            amount:       Amount::zero(),
+            receive_name: OwnedReceiveName::new_unchecked(
+                "smart_contract_oracle_integration.prices".to_string(),
+            ),
+            address:      contract_address,
+            message:      OwnedParameter::empty(),
+        })
+        .expect("Invoke view");
+
+    let rv: Vec<(String, u64)> = invoke.parse_return_value().expect("View return value");
+
+    println!("{:?}", rv);
+}
+
+/// Setup auction and chain.
+///
+/// Carol is the owner of the auction, which ends at `1000` milliseconds after
+/// the unix epoch. The 'microCCD per euro' exchange rate is set to `1_000_000`,
+/// so 1 CCD = 1 euro.
+fn initialize_chain_and_contract() -> (Chain, ContractAddress, ContractAddress) {
+    let mut chain = Chain::builder().build().expect("Exchange rate is in valid range");
+
+    // Create some accounts on the chain.
+    chain.create_account(Account::new(ALICE, ACC_INITIAL_BALANCE));
+
+    // Load and deploy the module.
+    let module = module_load_v1("concordium-out/module.wasm.v1").expect("Module exists");
+    let deployment = chain.module_deploy_v1(SIGNER, ALICE, module).expect("Deploy valid module");
+
+    // Initialize the oracle integration contract.
+    let init = chain
+        .contract_init(SIGNER, ALICE, Energy::from(10000), InitContractPayload {
+            amount:    Amount::zero(),
+            mod_ref:   deployment.module_reference,
+            init_name: OwnedContractName::new_unchecked(
+                "init_smart_contract_oracle_integration".to_string(),
+            ),
+            param:     OwnedParameter::empty(),
+        })
+        .expect("Initialize integration contract");
+
+    // Load and deploy the module.
+    let deployment_registry = chain
+        .module_deploy_v1(
+            Signer::with_one_key(),
+            ALICE,
+            module_load_v1("./umbrella-contract-modules/registry.wasm.v1")
+                .expect("`Umbrella_feeds.wasm.v1` module should be loaded"),
+        )
+        .expect("`Umbrella_feeds.wasm.v1` deployment should always succeed");
+
+    let initialization_registry = chain
+        .contract_init(Signer::with_one_key(), ALICE, Energy::from(10000), InitContractPayload {
+            amount:    Amount::zero(),
+            mod_ref:   deployment_registry.module_reference,
+            init_name: OwnedContractName::new_unchecked("init_registry".to_string()),
+            param:     OwnedParameter::empty(),
+        })
+        .expect("Initialization of `registry` should always succeed");
+
+    // Deploy 'umbrella_feeds' contract
+
+    let deployment = chain
+        .module_deploy_v1(
+            Signer::with_one_key(),
+            ALICE,
+            module_load_v1("./umbrella-contract-modules/umbrella_feeds.wasm.v1")
+                .expect("`Umbrella_feeds.wasm.v1` module should be loaded"),
+        )
+        .expect("`Umbrella_feeds.wasm.v1` deployment should always succeed");
+
+    let input_parameter_2 = InitParamsUmbrellaFeeds {
+        registry:            initialization_registry.contract_address,
+        required_signatures: 2,
+        staking_bank:        initialization_registry.contract_address, // Dummy value
+        decimals:            4,
+    };
+
+    let initialization_umbrella_feeds = chain
+        .contract_init(Signer::with_one_key(), ALICE, Energy::from(10000), InitContractPayload {
+            amount:    Amount::zero(),
+            mod_ref:   deployment.module_reference,
+            init_name: OwnedContractName::new_unchecked("init_umbrella_feeds".to_string()),
+            param:     OwnedParameter::from_serial(&input_parameter_2)
+                .expect("`InitContractsParam` should be a valid inut parameter"),
+        })
+        .expect("Initialization of `umbrella_feeds` should always succeed");
+
+    (chain, initialization_umbrella_feeds.contract_address, init.contract_address)
+}
