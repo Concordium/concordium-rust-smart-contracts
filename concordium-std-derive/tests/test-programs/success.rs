a1bc058032fa806de517455c0171c481e9d53067
rasmus-kirk <mail@rasmuskirk.com>
2024-03-12 14:56:59 +0100

    Added environment variable tests

--- concordium-std-derive/tests/test-programs/success.rs
+++ concordium-std-derive/tests/test-programs/success.rs
@@ -11,14 +11,20 @@ const PK_ECDSA: PublicKeyEcdsaSecp256k1 = public_key_ecdsa!("0214e6a60b8fc58ea70
 const SG_25519: SignatureEd25519 = signature_ed25519!("ec076ae7adaf0a8b921cf2bad86a1a5b5346226618637aa0d6b30f9616f108f9f482640a4ceb14235569cd3af05fac00be2c82dc81c6f6b4a6ba4ea7c3b51a0b");
 const SG_ECDSA: SignatureEcdsaSecp256k1 = signature_ecdsa!("EC076AE7ADAF0A8B921CF2BAD86A1A5B5346226618637AA0D6B30F9616F108F9F482640A4CEB14235569CD3AF05FAC00BE2C82DC81C6F6B4A6BA4EA7C3B51A0B");
 
-fn f() {
-    println!("{:?}", ACC.to_string());
-    println!("{:?}", REF.to_string());
-    println!("{:?}", CONTRACT.to_string());
-    println!("{:?}", PK_25519.to_string());
-    println!("{:?}", PK_ECDSA.to_string());
-    println!("{:?}", SG_25519.to_string());
-    println!("{:?}", SG_ECDSA.to_string()); 
-}
+const ACC_ENV: AccountAddress = account_address_env!("ACC");
+const REF_ENV: ModuleReference = module_reference_env!("REF");
+const CONTRACT_ENV: ContractAddress = contract_address_env!("CONTRACT");
+const PK_25519_ENV: PublicKeyEd25519 = public_key_ed25519_env!("PK_25519");
+const PK_ECDSA_ENV: PublicKeyEcdsaSecp256k1 = public_key_ecdsa_env!("PK_ECDSA");
+const SG_25519_ENV: SignatureEd25519 = signature_ed25519_env!("SG_25519");
+const SG_ECDSA_ENV: SignatureEcdsaSecp256k1 = signature_ecdsa_env!("SG_ECDSA");
 
-fn main() { }
+fn main() {
+    assert_eq!(ACC, ACC_ENV);
+    assert_eq!(REF, REF_ENV);
+    assert_eq!(CONTRACT, CONTRACT_ENV);
+    assert_eq!(PK_25519, PK_25519_ENV);
+    assert_eq!(PK_ECDSA, PK_ECDSA_ENV);
+    assert_eq!(SG_25519, SG_25519_ENV);
+    assert_eq!(SG_ECDSA, SG_ECDSA_ENV);
+}
