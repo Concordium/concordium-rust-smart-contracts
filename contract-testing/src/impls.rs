9cf2385c5a58726c207edb3955e5dc2f8d662ef2
Lasse Møldrup <lasse.moeldrup@gmail.com>
2024-05-21 16:01:10 +0200

    Fix cargo fmt

--- contract-testing/src/impls.rs
+++ contract-testing/src/impls.rs
@@ -2060,7 +2060,9 @@ pub fn module_load_v1(module_path: impl AsRef<Path>) -> Result<WasmModule, Modul
     Ok(module)
 }
 
-/// Load the current smart contract module output using the environment variable `CARGO_CONCORDIUM_TEST_MODULE_OUTPUT_PATH` which is set when running using `cargo concordium test`.
+/// Load the current smart contract module output using the environment variable
+/// `CARGO_CONCORDIUM_TEST_MODULE_OUTPUT_PATH` which is set when running using
+/// `cargo concordium test`.
 pub fn module_load_output() -> Result<WasmModule, OutputModuleLoadError> {
     let module_path = env::var(CONTRACT_MODULE_OUTPUT_PATH_ENV_VAR)?;
     let module = module_load_v1(module_path)?;
